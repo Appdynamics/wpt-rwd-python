a94c8df02203483a8edd687bea2e002ce197583a
Olivier Crameri <olivier.crameri@appdynamics.com>
2015-07-09 16:21:46 +0200

    Turn on Firefox and Internet Explorer

--- singleurl.py
+++ singleurl.py
@@ -3,6 +3,8 @@ import os
 import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 def get_chrome_caps(browser_args):
     chrome_options = Options()
@@ -12,13 +14,20 @@ def get_chrome_caps(browser_args):
     return chrome_options.to_capabilities()
     
 def get_firefox_caps(profile_dir):
-    raise NotYetImplemented()
+    firefox_capabilities = DesiredCapabilities.FIREFOX.copy()
+    # XXX this is probably not what we need
+    if (profile_dir):
+        firefox_capabilities['firefox_profile_dir'] = profile_dir
+    return firefox_capabilities
+
+def get_internetexplorer_caps():
+    return DesiredCapabilities.INTERNETEXPLORER.copy()
 
 parser = argparse.ArgumentParser(description='RWD client for single URL measurements')
 
 parser.add_argument('--server-url', required=True)
 parser.add_argument('--test-url', required=True)
-parser.add_argument('--browser', choices=['chrome', 'firefox', 'ie'], required=True)
+parser.add_argument('--browser', choices=['chrome', 'firefox', 'internet explorer'], required=True)
 parser.add_argument('--screenshot', default='screenshot.png')
 parser.add_argument('--firefox-profile-dir')
 parser.add_argument('browser_args', nargs='*')
@@ -32,6 +41,8 @@ if browser == "chrome":
     desired_capabilities = get_chrome_caps(args.browser_args)
 elif browser == "firefox":
     desired_capabilities = get_firefox_caps(args.firefox_profile_dir)
+elif browser == "internet explorer":
+    desired_capabilities = get_internetexplorer_caps()
 else:
     raise UnSupportedBrowser(browser);
 
@@ -47,5 +58,5 @@ driver.implicitly_wait(10)
 print("Requesting URL: " + args.test_url)
 driver.get(args.test_url)
 
-print("Done.")
 driver.quit()
+print("Done.")
