fec54971103dde0ffd61420ae98acc6687e2c870
Renault John Lecoultre <renault.lecoultre@appdynamics.com>
2015-07-14 16:29:45 +0200

    First version of runscript which execute arbitrary code from customer

---
+++ runscript.py
@@ -0,0 +1,20 @@
+from module.cliparser import build_parser
+from module.buildcap import build_capabilities
+from selenium import webdriver
+
+parser = build_parser('RWD client for running a script');
+parser.add_argument('--path', required=True)
+args = parser.parse_args()
+
+desired_capabilities = build_capabilities(args)
+desired_capabilities['name'] = 'Script: %s' % args.path
+
+print("Connecting to RWD server: " + args.server_url)
+driver = webdriver.Remote(desired_capabilities=desired_capabilities,
+                          command_executor=args.server_url)
+
+execfile(args.path, globals(), {driver: driver});
+
+# quit only if necessary
+driver.quit()
+print("Done.")
