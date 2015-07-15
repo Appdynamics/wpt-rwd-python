14fa4d61a7a72b41c7edd646158cad3a1f9cb84c
Renault John Lecoultre <renault.lecoultre@appdynamics.com>
2015-07-15 16:55:37 +0200

    No timeout to load page

--- runscript.py
+++ runscript.py
@@ -13,6 +13,11 @@ print("Connecting to RWD server: " + args.server_url)
 driver = webdriver.Remote(desired_capabilities=desired_capabilities,
                           command_executor=args.server_url)
 
+# with lock step, 'get' can take more time then a usual RWD call.
+# Setting page load timeout to a negative value will wait forever for the page to
+# load without throwing an exception
+driver.set_page_load_timeout(-1);
+
 execfile(args.path, globals(), {driver: driver});
 
 try:
