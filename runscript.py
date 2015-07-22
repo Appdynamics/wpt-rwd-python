4be6830e8d3704a70627ad89e4463f0900ff000c
Renault John Lecoultre <renault.lecoultre@appdynamics.com>
2015-07-22 14:11:25 +0200

    Address comments

--- runscript.py
+++ runscript.py
@@ -7,18 +7,17 @@ from module.buildcap import build_capabilities
 from selenium import webdriver
 
 # exit codes
-ERROR = 3
-UNSTABLE = 2
-FAILED = 1
-PASSED = 0
-ret = PASSED
+SCRIPT_ERROR = 3
+TEST_UNSTABLE = 2
+TEST_FAILED = 1
+TEST_PASSED = 0
+ret = TEST_PASSED
 
 parser = build_parser('RWD client for running a script');
+
+# path where the user script is located
 parser.add_argument('--path', required=True)
 
-parser.add_argument('--xml', dest='xml', action='store_true')
-parser.add_argument('--no-xml', dest='xml', action='store_false')
-parser.set_defaults(xml=True)
 args = parser.parse_args()
 
 desired_capabilities = build_capabilities(args)
@@ -31,11 +30,12 @@ driver = webdriver.Remote(desired_capabilities=desired_capabilities,
 # reset args list to have a pristine environment to execute the script
 sys.argv = ['test'];
 
-# script should not consider itself as main
+# For Unit Tests: script should not consider itself as main in order to prevent 
+# the provided script to run the testrunner by itself.
 globalScope = globals().copy();
 globalScope['__name__'] = 'script';
 
-localScope = {driver: driver}
+localScope = {'driver': driver}
 
 # with lock step, 'get' can take more time then a usual RWD call.
 # Setting page load timeout to a negative value will wait forever for the page to
@@ -48,9 +48,9 @@ except Exception as e:
     print str(e)
     if (isinstance(e, AssertionError)):
         # assertion = script failed
-        ret = FAILED
+        ret = TEST_FAILED
     else:
-        ret = ERROR
+        ret = SCRIPT_ERROR
 
 # automatically load all test classes to feed in test runner
 testSuite = unittest.TestSuite()
@@ -60,16 +60,13 @@ for item in localScope.itervalues():
 
 # If a suite was defined, run it
 if (testSuite.countTestCases() > 0):
-    if args.xml:
-        testRunner = xmlrunner.XMLTestRunner(verbosity=2, output='reports')
-    else:
-        testRunner = unittest.TextTestRunner(verbosity=2)
+    testRunner = xmlrunner.XMLTestRunner(verbosity=2, output='reports')
 
     results = testRunner.run(testSuite)
     if len(results.failures) > 0:
-        ret = FAILED
+        ret = TEST_FAILED
     elif len(results.errors) > 0:
-        ret = ERROR
+        ret = SCRIPT_ERROR
 
 # quit but don't report errors. The browser might be already closed.
 try:
