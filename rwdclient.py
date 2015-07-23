df48910c7252dbdb4e67b6e1d6d6a57ff7484b45
Renault John Lecoultre <renault.lecoultre@appdynamics.com>
2015-07-23 15:42:23 +0200

    Use a different error code

--- rwdclient.py
+++ rwdclient.py
@@ -8,6 +8,7 @@ from module.buildcap import build_capabilities
 from selenium import webdriver
 
 # exit codes
+SYSTEM_ERROR = 4
 SCRIPT_ERROR = 3
 TEST_UNSTABLE = 2
 TEST_FAILED = 1
@@ -20,7 +21,7 @@ desired_capabilities = build_capabilities(args)
 
 if (args.test_script and not(os.path.exists(args.test_script))):
     print "Error, file %s does not exist" % (args.test_script)
-    sys.exit(SCRIPT_ERROR);
+    sys.exit(SYSTEM_ERROR)
 
 print("Connecting to RWD server: " + args.server_url)
 driver = webdriver.Remote(desired_capabilities=desired_capabilities,
@@ -29,24 +30,24 @@ driver = webdriver.Remote(desired_capabilities=desired_capabilities,
 # with lock step, 'get' can take more time then a usual RWD call.
 # Setting page load timeout to a negative value will wait forever for the page to
 # load without throwing an exception
-driver.set_page_load_timeout(-1);
+driver.set_page_load_timeout(-1)
 
 driver.implicitly_wait(10)
 
 if args.test_url:
-    print('[%(id)s] Single URL measurement on %(url)s' % { 'id': args.id, 'url': args.test_url});
+    print('[%(id)s] Single URL measurement on %(url)s' % {'id': args.id, 'url': args.test_url})
     r = driver.get(args.test_url)
 else:
     print('[%s] Running a script' % args.id)
 
     # reset args list to have a pristine environment to execute the script
-    sys.argv = ['test'];
+    sys.argv = ['test']
 
     testSuite = unittest.TestSuite()
     scriptScope = {'driver': driver, 'testSuite': testSuite}
 
     try:
-        execfile(args.test_script, scriptScope, scriptScope);
+        execfile(args.test_script, scriptScope, scriptScope)
     except Exception as e:
         print str(e)
         if (isinstance(e, AssertionError)):
